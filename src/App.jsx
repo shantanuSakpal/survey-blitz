df476de8e161cb569851c0aee160d4bf9b4b12dd
meetp90 <meetpithadia14@gmail.com>
2023-06-07 17:55:12 +0530

    admin basic functionality init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './stylesheets/index.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";  
+import './App.css';
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
 
 function App() {
   return (
-    <div className="pages-container" id="page">
-     shantanu
-
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<>homepage</>} />
+      <Route exact path="/signIn" element={<SignIn />} />
+      <Route exact path="/signUp" element={<SignUp />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,49 @@
+import React,{useState} from 'react'
+import '../stylesheets/signin.scss'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function SignIn() {
+
+  const navigate = useNavigate();
+
+
+  const [inputValues, setInputValues] = useState({
+    email: '', password: ''
+  });
+  
+  const handleOnChange = event => {
+    const { name, value } = event.target;
+    setInputValues({ ...inputValues, [name]: value });
+  };
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({inputValues});
+    axios.post('http://localhost:3001/admin/signIn', inputValues)
+    .then(res => {
+      console.log(res.data);
+      navigate('/')
+    })
+    .catch(err => {
+      console.log(err);
+      navigate('/signUp')
+    })
+  }
+  return (
+    <>
+      <div className="header">
+        <h1>Signin</h1>
+      </div>
+      <div className="userInput">
+        <input type="text" name="email" placeholder='email' onChange={handleOnChange} />
+        <input type="text" name="password" placeholder='password' onChange={handleOnChange}  />
+      </div>
+      <div className="submitButton">
+        <button onClick={handleSubmit}>sign in</button>
+      </div>
+    </>
+  )
+}
+
+export default SignIn
