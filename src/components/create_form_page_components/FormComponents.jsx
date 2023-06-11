1619694be35163b39f31a0be65da983d22934129
meetp90 <meetpithadia14@gmail.com>
2023-06-11 10:56:05 +0530

    Dnd for sections

--- src/components/create_form_page_components/FormComponents.jsx
+++ src/components/create_form_page_components/FormComponents.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState } from "react";
 import { AddSectionButton } from "../buttons/AddSectionButton";
 import { SectionComponent } from "./SectionComponent";
 import AddInput from "./AddInput";
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 
 function FormComponents({
   formComponentsArray,
@@ -17,13 +18,21 @@ function FormComponents({
 
   const [currSectionId, setCurrSectionId] = useState(null);
 
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const items = Array.from(formComponentsArray);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setFormComponentsArray(items);
+  };
+
   return (
     <>
       <AddInput
         currSectionId={currSectionId}
         setFormComponentsArray={setFormComponentsArray}
       />
-      <div className="  left-column-container">
+      <div className="left-column-container">
         <div
           style={{
             display: "flex",
@@ -34,26 +43,49 @@ function FormComponents({
         >
           <h3>Components</h3>
         </div>
-
-        {formComponentsArray && (
-          <div className="form-components-container">
-            {formComponentsArray.map((component, index) => {
-              return (
-                <div key={index}>
-                  <SectionComponent
-                    section_id={component.section_id}
-                    removeSection={removeSection}
-                    formComponentsArray={formComponentsArray}
-                    index={index}
-                    setCurrSectionId={setCurrSectionId}
-                    setFormComponentsArray={setFormComponentsArray}
-                  />
-                </div>
-              );
-            })}
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="form-components-container">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="form-components-container"
+              >
+                {formComponentsArray &&
+                  formComponentsArray.map((component, index) => {
+                    return (
+                      <Draggable
+                        key={component.section_id}
+                        draggableId={component.section_id.toString()}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                          >
+                            <div key={index}>
+                              <SectionComponent
+                                section_id={component.section_id}
+                                removeSection={removeSection}
+                                formComponentsArray={formComponentsArray}
+                                index={index}
+                                setCurrSectionId={setCurrSectionId}
+                                setFormComponentsArray={setFormComponentsArray}
+                              />
+                            </div>
+                          </div>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                {provided.placeholder}
             <AddSectionButton addSection={addSection} />
           </div>
         )}
+      </Droppable>
+        </DragDropContext>
       </div>
     </>
   );
