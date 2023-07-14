03c9f0cb53ced37619fc5f9915e6a17fcfd4ce5f
meet-punctualiti <meet.p@punctualiti.com>
2023-07-14 18:26:38 +0530

    pushed sendOtp updated

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -13,13 +13,13 @@ const emailIsValid = require('../helpers/helper').emailIsValid;
 const passwordIsValid = require('../helpers/helper').passwordIsValid;
 
 // signUp route + send otp
-router.post('/signUp', async (req, res) => {    
+router.post('/signUp', async (req, res) => {
     try {
-        const { email, password,username } = req.body;
-        if(!emailIsValid(email))
+        const { email, password, username } = req.body;
+        if (!emailIsValid(email))
             throw new Error('email is not valid');
-    
-        if(!passwordIsValid(password))
+
+        if (!passwordIsValid(password))
             throw new Error('password must be atleast 6 characters long');
         const existingUser = await Admin.findOne({ email });
         if (existingUser)
@@ -31,131 +31,131 @@ router.post('/signUp', async (req, res) => {
             username,
             password: hashedPassword,
         });
-        const token = jwt.sign({email : admin.email ,id : admin._id }, SECRET_KEY);
-        res.status(200).json({ result: admin, token : token });
+        const token = jwt.sign({ email: admin.email, id: admin._id }, SECRET_KEY);
+        res.status(200).json({ result: admin, token: token });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
 router.post('/signIn', async (req, res) => {
-    const { email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
         const existingUser = await Admin.findOne({ email });
         if (!existingUser)
             return res.status(404).json({ message: "User doesn't exist" });
 
-        const isPasswordCorrect = await bcrypt.compare( password, existingUser.password );
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if (!isPasswordCorrect)
             return res.status(400).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({email : existingUser.email ,id : existingUser._id }, SECRET_KEY);
-        res.status(200).json({ result: existingUser, token : token });
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, SECRET_KEY);
+        res.status(200).json({ result: existingUser, token: token });
 
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong' });
     }
 });
 
-    router.post('/createForm', async (req, res) => {
-        const { email, token, formObject } = req.body;
-        try {
-            jwt.verify(token, SECRET_KEY, async (err) => {
-                if (err) {
-                    return res.status(401).json({ message: 'Unauthorized' });
-                } else {
-                    const admin = await Admin.findOne({ email });
-                    if (!admin)
-                        return res.status(404).json({ message: "User doesn't exist" });
+router.post('/createForm', async (req, res) => {
+    const { email, token, formObject } = req.body;
+    try {
+        jwt.verify(token, SECRET_KEY, async (err) => {
+            if (err) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            } else {
+                const admin = await Admin.findOne({ email });
+                if (!admin)
+                    return res.status(404).json({ message: "User doesn't exist" });
 
-                    // Check if a form with the same name already exists for the admin
-                    const existingForm = await Form.findOne({
-                        admin_id: admin._id,
-                        'formObject.form_name': formObject.form_name,
-                    });
-                    if (existingForm) {
-                        return res
-                            .status(400)
-                            .json({ message: 'Form with the same name already exists' });
-                    }
-
-                    admin.form_id.push(formObject.form_id);
-                    await admin.save();
-
-                    formObject.url =
-                        '/' + admin._id + '/' + _.kebabCase(formObject.form_name);
-                    const form = await Form.create({
-                        admin_id: admin._id,
-                        form_id: formObject.form_id,
-                        formObject: formObject,
-                    });
-                    res.status(200).json({ result: form,message: 'Form successfully created !' });
+                // Check if a form with the same name already exists for the admin
+                const existingForm = await Form.findOne({
+                    admin_id: admin._id,
+                    'formObject.form_name': formObject.form_name,
+                });
+                if (existingForm) {
+                    return res
+                        .status(400)
+                        .json({ message: 'Form with the same name already exists' });
                 }
-            });
-        } catch (error) {
-            res.status(500).json({ message: 'Something went wrong' });
-        }
-    });
 
-    // Update form API endpoint
-    router.post('/updateForm', async (req, res) => {
-        const { email, token, formObject } = req.body;
-        try {
-            jwt.verify(token, SECRET_KEY, async (err) => {
-                if (err) {
-                    return res.status(401).json({ message: 'Unauthorized' });
-                } else {
-                    const admin = await Admin.findOne({ email });
-                    if (!admin) {
-                        return res.status(404).json({ message: "User doesn't exist" });
-                    }
+                admin.form_id.push(formObject.form_id);
+                await admin.save();
 
-                    // Find the form by form name and admin ID
-                    const form = await Form.findOne({
-                        admin_id: admin._id,
-                        'formObject.form_name': formObject.form_name,
-                    });
+                formObject.url =
+                    '/' + admin._id + '/' + _.kebabCase(formObject.form_name);
+                const form = await Form.create({
+                    admin_id: admin._id,
+                    form_id: formObject.form_id,
+                    formObject: formObject,
+                });
+                res.status(200).json({ result: form, message: 'Form successfully created !' });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+});
 
-                    if (!form) {
-                        return res.status(404).json({ message: "Form doesn't exist" });
-                    }
-
-                    // Update the formObject with the new formObject
-                    form.formObject = formObject;
-                    await form.save();
-
-                    res.status(200).json({ result: form,message: 'Form successfully updated !' });
+// Update form API endpoint
+router.post('/updateForm', async (req, res) => {
+    const { email, token, formObject } = req.body;
+    try {
+        jwt.verify(token, SECRET_KEY, async (err) => {
+            if (err) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            } else {
+                const admin = await Admin.findOne({ email });
+                if (!admin) {
+                    return res.status(404).json({ message: "User doesn't exist" });
                 }
-            });
-        } catch (error) {
-            res.status(500).json({ message: 'Something went wrong' });
-        }
-    });
 
+                // Find the form by form name and admin ID
+                const form = await Form.findOne({
+                    admin_id: admin._id,
+                    'formObject.form_name': formObject.form_name,
+                });
 
-    // api to get all forms of a particular admin
-    router.post('/getForms', async (req, res) => {
-        const { email, token } = req.body;
-
-        try {
-            jwt.verify(token, SECRET_KEY, async (err) => {
-                if (err) {
-                    return res.status(401).json({ message: 'Unauthorized' });
-                } else {
-                    const admin = await Admin.findOne({ email });
-                    if (!admin)
-                        return res.status(404).json({ message: "User doesn't exist" });
-                    const forms = await Form.find({ admin_id: admin._id });
-                    res.status(200).json({ forms: forms });
+                if (!form) {
+                    return res.status(404).json({ message: "Form doesn't exist" });
                 }
-            });
-        } catch (error) {
-            res.status(500).json({ message: 'Something went wrong' });
-        }
-    });
 
-    //delete form
+                // Update the formObject with the new formObject
+                form.formObject = formObject;
+                await form.save();
+
+                res.status(200).json({ result: form, message: 'Form successfully updated !' });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+});
+
+
+// api to get all forms of a particular admin
+router.post('/getForms', async (req, res) => {
+    const { email, token } = req.body;
+
+    try {
+        jwt.verify(token, SECRET_KEY, async (err) => {
+            if (err) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            } else {
+                const admin = await Admin.findOne({ email });
+                if (!admin)
+                    return res.status(404).json({ message: "User doesn't exist" });
+                const forms = await Form.find({ admin_id: admin._id });
+                res.status(200).json({ forms: forms });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+});
+
+//delete form
 router.post('/deleteForm', async (req, res) => {
     const { form_id, admin_id, token } = req.body;
 
@@ -220,37 +220,37 @@ router.post('/changePassword', async (req, res) => {
 
 
 router.post('/getFormById', async (req, res) => {
-       const {form_id, token} = req.body;
-       try {
+    const { form_id, token } = req.body;
+    try {
         jwt.verify(token, SECRET_KEY, async (err) => {
             if (err) {
                 return res.status(401).json({ message: 'Unauthorized' });
             } else {
-                const form = await Form.findOne({"form_id":form_id});
+                const form = await Form.findOne({ "form_id": form_id });
                 if (!form)
-                    return res.status(404).json({ message: "Form doesn't exist" }); 
+                    return res.status(404).json({ message: "Form doesn't exist" });
                 res.status(200).json({ form: form });
             }
         });
-       } catch(e) { 
-              return res.status(500).json({ message: 'Something went wrong' });
-       }
-    });
+    } catch (e) {
+        return res.status(500).json({ message: 'Something went wrong' });
+    }
+});
 
 
 router.post("/sendOTP", async (req, res) => {
-    try{
-        const {email} = req.body;
+    try {
+        const { email } = req.body;
         const otp = helper.generateOTP();
-        if(!otp)
+        if (!otp)
             throw new Error('OTP generation failed');
-        const result = await axios.post("https://script.google.com/macros/s/AKfycbxyhUYYyRXeHLXmkXwuAHeqwrGuxvI_xmlLDZ15S4bTOCw8qUVh-fFFb6q4kUwGDvlV/exec",{
-            "email" : email,
-            "otp" : otp,
-            "msg"    : "Hello, " + email + " your OTP is " + otp + " ." +" Thank you for using our service."
+        const result = await axios.post("https://script.google.com/macros/s/AKfycbxyhUYYyRXeHLXmkXwuAHeqwrGuxvI_xmlLDZ15S4bTOCw8qUVh-fFFb6q4kUwGDvlV/exec", {
+            "email": email,
+            "otp": otp,
+            "msg": "Hello, " + email + " your OTP is " + otp + " ." + " Thank you for using our service."
         })
-        res.status(200).json({message : "OTP sent successfully"}); 
-    } catch(error){
+        res.status(200).json({ otp: otp, message: "OTP sent successfully" });
+    } catch (error) {
         res.status(500).json({ error: error.message });
     }
 })
