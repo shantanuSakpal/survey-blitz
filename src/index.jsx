82c7c24c077728858083441419789979375f9d58
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-09-17 00:39:11 +0530

    working

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import {store} from "./store/store";
-import 'react-toastify/dist/ReactToastify.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import "react-toastify/dist/ReactToastify.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "dotenv";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const clientId = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID;
 root.render(
-    <GoogleOAuthProvider clientId = "325890672365-2mjhq502oc5udfd96pdfc2h66okjkjku.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={clientId}>
     <BrowserRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-    </GoogleOAuthProvider>
+  </GoogleOAuthProvider>
 );
