852cb8720541015211faa0cdaebc30197bc9233b
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-09-17 12:26:38 +0530

    allowed origins update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://surveyblitz.vercel.app",
-  "https://surveyblitz-api.onrender.com",
-  "https://surveyblitz.onrender.com",
-];
+const allowedOrigins = ["https://surveyblitz.onrender.com"];
 
 module.exports = allowedOrigins;
