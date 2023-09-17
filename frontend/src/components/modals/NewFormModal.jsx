75c59b7ae1bf977a420bab752a507145bf7c8e15
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-09-18 00:07:49 +0530

    added read me and some other changes

--- frontend/src/components/modals/NewFormModal.jsx
+++ frontend/src/components/modals/NewFormModal.jsx
@@ -40,7 +40,7 @@ export const NewFormModal = ({ setModalOpen }) => {
     const adminId = user.result._id;
     const formattedFormName = _.kebabCase(formName);
     const randomString = Math.random().toString(36).substring(2, 7);
-    const baseUrl = "http://localhost:3000"; //
+    const baseUrl = "https://surveyblitz.onrender.com"; //
     formUrl = `${baseUrl}/${adminId}/${formattedFormName + "-" + randomString}`;
     console.log(formUrl);
   }
