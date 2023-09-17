74482e1c7fc961e5ff825ef42f5f9ddf5e3d11bd
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-09-17 13:06:53 +0530

    remved cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["https://surveyblitz.onrender.com"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://surveyblitz.vercel.app",
+  "https://surveyblitz-api.onrender.com",
+  "https://surveyblitz.onrender.com",
+];
 
 module.exports = allowedOrigins;
